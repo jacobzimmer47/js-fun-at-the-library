096c1ff5aece9be1eda12b5345f7408f2be3a678
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-08-25 19:30:19 -0600

    Built every function expect the last' ' ' ''

--- src/book.js
+++ src/book.js
@@ -12,17 +12,43 @@ function buildMainCharacter(name, age, pronouns) {
   return character
 } 
 
+function saveReview(customerReview, reviews) {
+  if (!reviews.includes(customerReview)){
+    reviews.push(customerReview)
+  }
+  return reviews
+}
+
+function calculatePageCount(bookTitle) {
+for(var i = 0; i < bookTitle.length; i++){
+ return bookTitle.length * 20
+
+}
+
+}
 
 
 
 
+function writeBook(title, bookCharacter, genre) {
+  var book = {
+    title: title,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  }
+  return book
+}
 
+function editBook(newPageCount) {
+
+  }
 
 module.exports = {
   createTitle,
   buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
