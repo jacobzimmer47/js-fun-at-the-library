99846fe92b32cc8b1a8a7620cedb1095f84e1aa4
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-08-30 12:49:28 -0600

    Feat: library functions

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,24 @@
+function createLibrary (name) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+  return library
+}
 
+function addBook (book) {
+  if (book.genre === 'fantasy' ) {
+    fantasy.push(book)
+  }
+  return book
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+  createLibrary,
+  addBook,
   // checkoutBook
 };
